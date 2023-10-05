f85c6e50768de596186663c53fd82681ee088881
Your Name <tragger25@gmail.com>
2023-10-05 13:37:45 -0400

    added redux persist

--- my-tasty-app/src/store.js
+++ my-tasty-app/src/store.js
@@ -4,16 +4,26 @@ import productReducer from './features/ProductSlice'
 import filterReducer from './features/FilterSlice'
 import queryReducer from './features/QuerySlice'
 import favouriteReducer from './features/FavouriteSlice'
-
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
 import userReducer from './features/UserSlice'
-export const  store = configureStore({
- reducer:{
-  cart:cartReducer,
-  filter:filterReducer,
-  products:productReducer,
-  query:queryReducer,
-  favourite:favouriteReducer,
-  user:userReducer,
- },
- devTools: true
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    filter: filterReducer,
+    products: productReducer,
+    query: queryReducer,
+    favourite: favouriteReducer,
+    user: persistedReducer,
+  },
+  devTools: true,
 })
+export const persistor = persistStore(store)
